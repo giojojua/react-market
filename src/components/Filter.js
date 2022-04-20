152eb2c11e62c2140dfdff983a7840820f707e2f
Gio <jojuagio7@gmail.com>
2022-04-20 13:53:40 +0400

    product page strapi fetching

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,12 +1,16 @@
 import React from "react";
 import {Col, Row} from "react-bootstrap";
 import {MdKeyboardArrowDown} from "react-icons/md";
+import useFetch from "../hooks/useFetch";
 
 export default function Filter() {
 
-    const product = require('../data/data.json').product
+    const { loading, error, data } = useFetch('http://localhost:1337/api/products?populate=%2A')
 
-    product.sort((a, b) => a.position > b.position ? 1 : -1)
+    if (loading) return <p>Loading...</p>
+    if (error) return <p>Error...</p>
+
+    // product.sort((a, b) => a.position > b.position ? 1 : -1)
 
     function DropdownToggle() {
         if (document.getElementById('filter_dropdown').style.display === 'block') {
@@ -43,11 +47,14 @@ export default function Filter() {
                         <ul id="filter_dropdown" style={{display: "none"}}>
                             <li className="list" data-category-title="all" onClick={categoryFilter}>All Categories</li>
                             {
-                                product.map(product => {
+                                data.data.map(product => {
+                                    let attribute = product.attributes
+                                    let category = attribute.product_category.data.attributes.name
+
                                     return (
-                                        <li className="list" key={product.id} data-category-title={product.category}
+                                        <li className="list" key={product.id} data-category-title={category}
                                             onClick={categoryFilter}>
-                                            {product.category}
+                                            {category}
                                         </li>
                                     )
                                 })
--- src/pages/Product.js
+++ src/pages/Product.js
@@ -5,13 +5,11 @@ import useFetch from "../hooks/useFetch";
 
 export default function Product() {
 
-    const { loading, error, data } = useFetch('http://localhost:1337/api/products')
+    const {loading, error, data} = useFetch('http://localhost:1337/api/products?populate=%2A')
 
     if (loading) return <p>Loading...</p>
     if (error) return <p>Error...</p>
 
-    // const product = require('../data/data.json').product
-    //
     // product.sort((a, b) => a.position > b.position ? 1 : -1)
 
 
@@ -20,15 +18,20 @@ export default function Product() {
             {
                 data.data.map(product => {
                     let attribute = product.attributes
-                    console.log(attribute.image)
+                    let image = attribute.image.data.attributes.url
+                    let category = attribute.product_category.data.attributes.name
+
                     return (
-                        <Col xs={12} sm={6} md={6} lg={3} key={product.id} className="category" data-item={attribute.category}>
+                        <Col xs={12} sm={6} md={6} lg={3} key={product.id} className="category" data-item={category}>
                             <Link to="/pages/productdetailed"
-                                  state={{ from: product.id }}
+                                  state={{from: product.id}}
                                   className="text-decoration-none">
                                 <div className="box">
                                     <div className="boximage">
-                                        <img src={attribute.image} alt=""/>
+                                        <div className="category-baner">
+                                            {category}
+                                        </div>
+                                        <img src={"http://localhost:1337" + image} alt=""/>
                                     </div>
                                     <div className="boxtext">
                                         <h5>{attribute.title} {attribute.model}</h5>
@@ -51,39 +54,5 @@ export default function Product() {
                 })
             }
         </Row>
-
-        // <Row className="py-5">
-        //     {
-        //         product.map(product => {
-        //             return (
-        //                 <Col xs={12} sm={6} md={6} lg={3} key={product.id} className="category" data-item={product.category}>
-        //                     <Link to="/pages/productdetailed"
-        //                           state={{ from: product.id }}
-        //                           className="text-decoration-none">
-        //                         <div className="box">
-        //                             <div className="boximage">
-        //                                 <img src={product.image} alt=""/>
-        //                             </div>
-        //                             <div className="boxtext">
-        //                                 <h5>{product.title} {product.model}</h5>
-        //                                 <hr/>
-        //                                 <div className="d-flex justify-content-between">
-        //                                     <div>
-        //                                         <span className="pe-2">Price:</span>
-        //                                         <span className="text-white">{product.price} BTC</span>
-        //                                     </div>
-        //                                     <div>
-        //                                         <span className="pe-2">Year:</span>
-        //                                         <span className="text-white">{product.year}</span>
-        //                                     </div>
-        //                                 </div>
-        //                             </div>
-        //                         </div>
-        //                     </Link>
-        //                 </Col>
-        //             )
-        //         })
-        //     }
-        // </Row>
     )
 }
